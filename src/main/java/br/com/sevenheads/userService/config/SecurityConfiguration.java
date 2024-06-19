6cd89fac94fcc5d9289607b2f6514b5af5035a55
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-06-19 01:52:01 -0300

    Try

--- src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
+++ src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
@@ -3,8 +3,12 @@ package br.com.sevenheads.userService.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +22,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration {
 	
 	@Autowired
@@ -25,7 +30,10 @@ public class SecurityConfiguration {
 	
 	@Autowired
 	private AuthenticationProvider authenticationProvider;
-	
+
+	public String crossOriginAllowedHeaders="header1,header2, *" ;
+	public String crossOriginAllowedSites="site1,site2, * ";
+
 	@SuppressWarnings("removal")
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,7 +42,7 @@ public class SecurityConfiguration {
 				.and()
 				.csrf()
 				.disable()
-				/*.authorizeHttpRequests(authorizeConfig -> {
+				.authorizeHttpRequests(authorizeConfig -> {
 					authorizeConfig.requestMatchers("/swagger-ui.html").permitAll();
 					authorizeConfig.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
 					authorizeConfig.requestMatchers("/v1/api/auth/**").permitAll();
@@ -45,19 +53,32 @@ public class SecurityConfiguration {
 				})
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
+				.headers()
+				.frameOptions()
+				.sameOrigin().addHeaderWriter((request,response)->{
+					response.setHeader("Cache-Control","no-cache, no-store, max-age=0, must-revalidate, private");
+					response.setHeader("Pragma","no-cache");
+					response.setHeader("Access-Control-Allow-Origin",this.crossOriginAllowedSites);
+				})
+				.and()
 				.authenticationProvider(authenticationProvider)
-				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)*/
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
 
 	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-		configuration.setExposedHeaders(Arrays.asList("Authorization"));
-		configuration.setAllowCredentials(true);
+		configuration.setAllowCredentials(false);
+		configuration.setAllowedHeaders(Arrays.asList(crossOriginAllowedHeaders.split(",")));
+		configuration.setAllowedOrigins(Arrays.asList(crossOriginAllowedSites.split(",")));
+		configuration.addAllowedMethod(HttpMethod.OPTIONS);
+		configuration.addAllowedMethod(HttpMethod.GET);
+		configuration.addAllowedMethod(HttpMethod.POST);
+		configuration.addAllowedMethod(HttpMethod.PUT);
+		configuration.addAllowedMethod(HttpMethod.DELETE);
+		configuration.addExposedHeader("Authorization");
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
