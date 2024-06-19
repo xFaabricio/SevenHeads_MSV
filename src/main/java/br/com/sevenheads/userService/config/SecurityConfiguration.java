eaf36c9dbdce2609006ff1c089332e7b9e4cd2ab
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-06-19 00:45:37 -0300

    Cors configuration

--- src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
+++ src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
@@ -13,6 +13,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -50,9 +52,14 @@ public class SecurityConfiguration {
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        source.registerCorsConfiguration("/**", configuration);
         return source;
 	}
-	
+
 }
