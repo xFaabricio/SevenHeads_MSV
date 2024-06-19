15f6beeeabf407503c018c91f91b2f2628be465c
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-06-19 02:45:58 -0300

    Agora vai

--- src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
+++ src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
@@ -53,12 +53,6 @@ public class SecurityConfiguration {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-		configuration.setExposedHeaders(Arrays.asList("Authorization"));
-		configuration.setAllowCredentials(true);
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
 	}
