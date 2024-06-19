352a888ee5f1524ac4acf608071ee8785538f831
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-06-19 02:04:14 -0300

    Another try

--- src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
+++ src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package br.com.sevenheads.userService.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,8 +12,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -28,39 +25,35 @@ public class SecurityConfiguration {
 
 	@SuppressWarnings("removal")
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http
-				.cors()
-				.and()
-				.csrf()
-				.disable()
-				.authorizeHttpRequests(authorizeConfig -> {
-					authorizeConfig.requestMatchers("/swagger-ui.html").permitAll();
-					authorizeConfig.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
-					authorizeConfig.requestMatchers("/v1/api/auth/**").permitAll();
-					authorizeConfig.requestMatchers("/v1/formService/**").permitAll();
-					authorizeConfig.requestMatchers("/server/**").permitAll();
-					authorizeConfig.requestMatchers("/logout").permitAll();
-					authorizeConfig.anyRequest().authenticated();
-				})
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-				.authenticationProvider(authenticationProvider)
-				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-				.build();
-	}
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+        		.cors()
+        		.and()
+        		.csrf()
+        		.disable()
+                .authorizeHttpRequests(
+                		authorizeConfig -> {
+                			authorizeConfig.requestMatchers("/swagger-ui.html").permitAll();
+                			authorizeConfig.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
+                			authorizeConfig.requestMatchers("/v1/api/auth/**").permitAll();
+							authorizeConfig.requestMatchers("/v1/api/auth/authenticate").permitAll();
+                			authorizeConfig.requestMatchers("/v1/formService/**").permitAll();
+                			authorizeConfig.requestMatchers("/server/**").permitAll();
+                			authorizeConfig.requestMatchers("/logout").permitAll();
+                			authorizeConfig.anyRequest().authenticated();
+                		})
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setMaxAge(Long.MAX_VALUE);
-		configuration.setAllowCredentials(true);
-		configuration.addAllowedHeader("*");
-		configuration.addAllowedOrigin("*");
-		configuration.addAllowedMethod("*");
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
 	}
 
 }
