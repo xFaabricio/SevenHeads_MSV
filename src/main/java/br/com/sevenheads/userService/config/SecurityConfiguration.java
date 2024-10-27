5fc6b5d34aab4f7b58c1b58f7eb2afec68acf95b
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-10-27 12:37:44 -0300

    Tentando fazer funcionar

--- src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
+++ src/main/java/br/com/sevenheads/userService/config/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("*"));
+		configuration.setAllowedOriginPatterns(List.of("*"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
 		configuration.setAllowCredentials(true);
